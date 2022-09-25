6c8427e1e1df173f971e5223a40d35de9639f4f6
bkhanal87@gmail.com <bkhanal87@gmail.com>
2022-09-25 06:29:00 -0400

    create html and js files

---
+++ index.js
@@ -0,0 +1,17 @@
+// let getComputerChoice;
+
+// function getComputerChoice() {
+
+// }
+
+// function playRound(playerSelection, computerSelection) {
+
+// }
+
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+
+// function game() {
+
+// }
